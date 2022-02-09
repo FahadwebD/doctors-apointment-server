a9b996cc3e070ef7b95dfe8e1341a8966c48b9d8
FahadwebD <fahadchowdhury66779@gmail.com>
2022-02-09 21:56:22 +0600

    doctor api added

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function run (){
         await client.connect();
         const database = client.db('doctorPortal');
         const serviceCollection = database.collection('services');
-        
+        const usersCollection = database.collection('users');
 
         app.get('/services' , async(req , res)=>{
             const cursor = serviceCollection.find({});
@@ -29,6 +29,42 @@ async function run (){
             res.send(services)
         })
 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+
+          // doctors api
+          app.get('/doctors', async (req, res) => {
+            const cursor = doctorsCollection.find({});
+            const doctors = await cursor.toArray();
+            res.json(doctors);
+        });
+
+        app.get('/doctors/:id', async (req, res) => {
+            const query = { _id: ObjectId(req.params.id) }
+            const doctor = await doctorsCollection.findOne(query);
+            res.json(doctor);
+        });
+
+        app.post('/doctors', async (req, res) => {
+            const name = req.body.name;
+            const email = req.body.email;
+            const pic = req.files.image;
+            const picData = pic.data;
+            const encodedPic = picData.toString('base64');
+            const imageBuffer = Buffer.from(encodedPic, 'base64');
+            const doctor = {
+                name,
+                email,
+                image: imageBuffer
+            }
+            const result = await doctorsCollection.insertOne(doctor);
+            res.json(result);
+        })
 
 
     }
